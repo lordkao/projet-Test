b4173782cfc40b5b543bf814c3382c0d459e6c06
David Yem <david.yem.devweb@gmail.com>
2021-11-23 00:40:39 +0100

    correction bug

--- src/components/homepage/accueil/DiscoverHealth.js
+++ src/components/homepage/accueil/DiscoverHealth.js
@@ -7,12 +7,11 @@ const DiscoverHealth = ({isLoading,dataPrograms}) => {
 
     const headingTitle = 'découvrez nos programmes santé'.toUpperCase()
     
-    !isLoading&& console.log(dataPrograms[2].name)
+    !isLoading&& console.log(dataPrograms[0].illustrationUrl)
 
     
     return(
         <View style={styles.discoverHealth}>
-
             {
                 isLoading?
                     /*Logo de chargement en attente de recevoir les données*/
@@ -22,49 +21,32 @@ const DiscoverHealth = ({isLoading,dataPrograms}) => {
 
                         {/*Titre programme santé*/}
                         <Text style={styles.headingTitleHealth}>{headingTitle}</Text>
-
                         <View style={styles.frameLessons}>
 
                             {/*Cadre de gauche*/}
-                            <View style={[styles.frameContainer]}>
-                                <Frameprogram 
-                                    title={dataPrograms[0].name} 
-                                    urlImage={dataPrograms[0].illustrationUrl} 
-                                />
+                            <View style={styles.frameContainer}>
+                                <Frameprogram title={dataPrograms[0].name} urlImage={dataPrograms[0].illustrationUrl}/>
                             </View>
 
                             {/*Cadre de droite*/}
                             <View style={[styles.frameContainer,{justifyContent:'space-between'}]}>
-
                                 <View style={styles.frameLeft}>
-                                    <Frameprogram 
-                                        title={dataPrograms[1].name} 
-                                        urlImage={dataPrograms[1].illustrationUrl} 
-                                    />
+                                    <Frameprogram title={dataPrograms[1].name} urlImage={dataPrograms[1].illustrationUrl}/>
                                 </View>
-
                                 <View style={styles.frameLeft}>
-                                    <Frameprogram 
-                                        title={dataPrograms[2].name} 
-                                        urlImage={dataPrograms[2].illustrationUrl} 
-                                    />
+                                    <Frameprogram title={dataPrograms[2].name} urlImage={dataPrograms[2].illustrationUrl}/>
                                 </View>
-                                
                             </View>
-
                         </View>
-
-
+                        
                         {/*Bouton qui affiche tous les programmes*/}
                         <View style={styles.btnAllProgramsContainer}>
                             <TouchableOpacity activeOpacity={0.7} style={styles.btnAllPrograms}>
                                 <Text style={styles.btnContent}>Voir tous les programmes</Text>
                             </TouchableOpacity>
                         </View>
-
                     </View>
             }
-
         </View>
     )
 }
